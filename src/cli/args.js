49209c3df16adcacc4cc5ea08e3ca7e580fbccf3
kachadi <kachadiii@gmail.com>
2022-12-04 22:40:07 +0200

    feat: implement env and args functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+    let resultArr = [];
+    let miniArr = [];
+    process.argv.slice(2)
+        .forEach((val,id) => {
+            if (id === 0 || id % 2 === 0 ) {
+                miniArr.push(val)
+            } else {
+                miniArr.push(val);
+                resultArr.push(miniArr);
+                miniArr = [];
+            }
+        })
+    resultArr.forEach(val => {
+        console.log(`${val[0].slice(2)} is ${val[1]}`);
+    })
 };
 
 parseArgs();
